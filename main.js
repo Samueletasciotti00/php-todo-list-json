eda58c2a8f6245bba6c1cc9011493b8871bbeed5
Samuele <samueletasciotti@icloud.com>
2024-07-27 13:00:17 +0200

    feat: add to list the input

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ const { createApp } = Vue;
             return{
                 title: 'To do list',
                 api: 'server.php',
-                store: []
+                store: [],
+                itemLista: ''
             }
         },
         methods: {
@@ -18,9 +19,19 @@ const { createApp } = Vue;
                     this.store = res.data;
                     console.log(this.store);
                 })
-
-               
-               
+            },
+            insert(){
+                const data = {
+                    itemLista: this.itemLista
+                }
+                axios.post(this.api, data, {
+                    headers: { "Content-Type" : "multipart/form-data" }
+                })
+                    .then(res => {
+                        console.log(res.data);
+                        this.store = res.data;
+                    })
+                    this.itemLista = '';
             }
         },
         mounted(){
--- server.php
+++ server.php
@@ -4,10 +4,21 @@
 $json_string = file_get_contents('todo.json');
 
 // converto la stringa in file PHP
-$list = json_decode($json_string);
+$store = json_decode($json_string);
+
+if(isset($_POST['itemLista'])){
+    $itemLista = $_POST['itemLista'];
+
+    // Aggiungere il task nello store
+    $store[] = ['text' => $itemLista];
+    // Sovrascrittura del json per la lista aggiornata
+    file_put_contents('todo.json', json_encode($store));
+}
 
 
+
+// Soecifico il tipo di formato inviato
 header('Content-Type: application/json');
 
-
-echo json_encode($list);
+// Debug
+echo json_encode($store);
